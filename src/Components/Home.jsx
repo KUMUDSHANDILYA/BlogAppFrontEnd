d5b6935cedf7db6ebceeab8ffa125d20005b13b5
Kumud Shandilya <kumudshandilyag@gmail.com>
2021-01-23 16:50:31 +0530

    Header

---
+++ src/Components/Home.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function Home() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 3</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Home;
